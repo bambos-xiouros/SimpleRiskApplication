b0c52e9e4ed0f4dbde88a014cef41b8584a32b41
bambos-xiouros <bambos.xiouros@gmail.com>
2015-08-02 20:30:49 +1000

    Added AverageSteakRatio, this will be the basis for the rest of the Rules.

---
+++ SimpleRiskApplication/Converters/StakeMultiplierConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SimpleRiskApplication.Converters
+{
+    public class StakeMultiplierConverter : IMultiValueConverter
+    {
+        public static readonly IMultiValueConverter Instance = new StakeMultiplierConverter();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stake = double.Parse(values[0].ToString());
+            var averageStake = double.Parse(values[1].ToString());
+            return (stake/averageStake).ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
